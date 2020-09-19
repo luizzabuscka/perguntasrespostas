82de99353ff2ae940fc6df72dbce8b3e49f1f982
Luiz Zabuscka <luiz.zabuscka@supersim.com.br>
2020-09-19 04:25:33 -0300

    create connection to database in amazon

--- database/database.js
+++ database/database.js
@@ -1,8 +1,10 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
+const mysql2 = require("mysql2");
 
-const connection = new Sequelize({
-    dialect: 'sqlite',
-    storage: './database/db.sqlite'
-  });
+const connection = new Sequelize("perguntas_respostas", "chuntr", "chuntr1234", {
+  host: "chuntrdb.c1uzpsc3txer.sa-east-1.rds.amazonaws.com",
+  dialect: "mysql",
+  dialectModule: mysql2
+});
 
-  module.exports = connection;
+module.exports = connection;
