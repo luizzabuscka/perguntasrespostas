757b1d9610ef8f51ba1817241ce1a20890b91856
Luiz Zabuscka <luiz.zabuscka@supersim.com.br>
2020-11-18 19:33:20 -0300

    Change database

--- database/database.js
+++ database/database.js
@@ -1,10 +1,15 @@
 const Sequelize = require("sequelize");
 const mysql2 = require("mysql2");
 
-const connection = new Sequelize("perguntas_respostas", "chuntr", "chuntr1234", {
-  host: "chuntrdb.c1uzpsc3txer.sa-east-1.rds.amazonaws.com",
-  dialect: "mysql",
-  dialectModule: mysql2
+// const connection = new Sequelize("perguntas_respostas", "chuntr", "chuntr1234", {
+//   host: "chuntrdb.c1uzpsc3txer.sa-east-1.rds.amazonaws.com",
+//   dialect: "mysql",
+//   dialectModule: mysql2
+// });
+
+const connection = new Sequelize({
+  dialect: 'sqlite',
+  storage: './database/db.sqlite'
 });
 
 module.exports = connection;
